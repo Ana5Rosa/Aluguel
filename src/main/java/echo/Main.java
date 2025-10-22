da597b87a7c55adf5ff6cc5c41a8be56c5f5c5ee
Ana Rosa <anacla.ped.rosa@gmail.com>
2025-10-21 22:36:39 -0300

    script sonar

--- src/main/java/echo/Main.java
+++ src/main/java/echo/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         JavalinApp app = new JavalinApp();
         app.start(80);
-        System.out.println("Olá, mundo!");
+        System.out.println("Olá, olá!");
     }
 }
