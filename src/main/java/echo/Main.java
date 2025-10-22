40b3d504370606b5b099023bf27dba7e41f7711c
Ana Rosa <anacla.ped.rosa@gmail.com>
2025-10-21 22:34:16 -0300

    script sonar

--- src/main/java/echo/Main.java
+++ src/main/java/echo/Main.java
@@ -1,9 +1,11 @@
 package echo;
 import echo.util.JavalinApp;
+import sun.awt.windows.WPrinterJob;
 
 public class Main {
     public static void main(String[] args) {
         JavalinApp app = new JavalinApp();
         app.start(80);
+        System.out.println("Olá, mundo!");
     }
 }
